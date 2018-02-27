95990aa9c3f41dc7bb340bfb65a9f76aff6732eb
Kari Bancroft <kariabancroft@gmail.com>
2018-02-26 22:31:14 -0800

    more accurately set up passenger relationships to tie to trips and drivers

--- specs/passenger_spec.rb
+++ specs/passenger_spec.rb
@@ -2,7 +2,7 @@ require_relative 'spec_helper'
 
 describe "Passenger class" do
 
-  it "is an instance of RideShare::Passenger" do
+  it "is an instance of Passenger" do
     test_rider = RideShare::Passenger.new({id: 1, name: "Smithy", phone_number: "353-533-5334"})
     test_rider.must_be_kind_of RideShare::Passenger
   end
@@ -10,6 +10,9 @@ describe "Passenger class" do
   describe "trips property" do
     before do
       @passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone_number: "1-602-620-2330 x3723", trips: [])
+      trip = RideShare::Trip.new({id: 8, driver: nil, passenger: @passenger, date: "2016-08-08", rating: 5})
+
+      @passenger.add_trip(trip)
     end
 
     it "returns an array of trips" do
@@ -22,23 +25,28 @@ describe "Passenger class" do
       end
     end
 
-    it "all Trips must have the same RideShare::Passenger id" do
+    it "all Trips must have the same Passenger id" do
       @passenger.trips.each do |trip|
-        trip.passenger_id.must_equal 9
+        trip.passenger.id.must_equal 9
       end
     end
   end
 
   describe "get_drivers method" do
     before do
-      trips = [RideShare::Trip.new(id: 1, )]
       @passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone_number: "1-602-620-2330 x3723")
+      driver = RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678")
+      trip = RideShare::Trip.new({id: 8, driver: driver, passenger: @passenger, date: "2016-08-08", rating: 5})
+
+      @passenger.add_trip(trip)
     end
 
     it "returns an array" do
-      @passenger.get_drivers.must_be_kind_of Array
+      drivers = @passenger.get_drivers
+      drivers.must_be_kind_of Array
+      drivers.length.must_equal 1
     end
-    
+
     it "all items in array are Driver instances" do
       @passenger.get_drivers.each do |driver|
         driver.must_be_kind_of RideShare::Driver
