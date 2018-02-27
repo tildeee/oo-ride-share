bd0725606a6b38ff183a5f43e3fad91fe0df972a
Kari Bancroft <kariabancroft@gmail.com>
2018-02-26 21:32:56 -0800

    cleanup driver class. update to accept trips list and add trip method. calculate average appropriately based on trip length

--- lib/driver.rb
+++ lib/driver.rb
@@ -3,32 +3,41 @@ require_relative 'trip'
 
 module RideShare
   class Driver
-    attr_reader :id, :name, :vehicle_id
+    attr_reader :id, :name, :vehicle_id, :trips
 
-    def initialize(id, name, vin)
-      if id == nil || id <= 0
+    def initialize(input)
+      if input[:id] == nil || input[:id] <= 0
         raise ArgumentError.new("ID cannot be blank or less than zero. (got #{id})")
       end
 
-      @id = id
-      @name = name
-      @vehicle_id = vin
-    end
+      @id = input[:id]
+      @name = input[:name]
+      @vehicle_id = input[:vin]
 
-    def get_trips
-      trips = Trip.all_for_driver(@id)
-      return trips
+      @trips = input[:trips] == nil ? [] : input[:trips]
     end
 
     def average_rating
-      trips = get_trips
       total_ratings = 0
-      trips.each do |trip|
+      @trips.each do |trip|
         total_ratings += trip.rating
       end
-      average = total_ratings / trips.length
+
+      if trips.length == 0
+        average = 0
+      else
+        average = (total_ratings.to_f) / trips.length
+      end
 
       return average
+    end
+
+    def add_trip(trip)
+      if trip.class != Trip
+        raise ArgumentError.new("Can only add trip instance to trip collection")
+      end
+
+      @trips << trip
     end
   end
 end
--- specs/driver_spec.rb
+++ specs/driver_spec.rb
@@ -4,39 +4,44 @@ describe "Driver class" do
 
   describe "Driver instantiation" do
     it "is an instance of Driver" do
-      test_driver = RideShare::Driver.new(1, "George", 3313)
+      test_driver = RideShare::Driver.new(id: 1, name: "George", vin: 3313)
       test_driver.must_be_kind_of RideShare::Driver
     end
     it "throws an argument error with a bad ID value" do
-      proc{ RideShare::Driver.new(0, "George", 3313)}.must_raise ArgumentError
+      proc{ RideShare::Driver.new(id: 0, name: "George", vin: 3313)}.must_raise ArgumentError
+    end
+    it "sets trips to an empty array if not provided" do
+      test_driver = RideShare::Driver.new(id: 1, name: "George", vin: 3313)
+      test_driver.trips.must_be_kind_of Array
+      test_driver.trips.length.must_equal 0
     end
   end
 
-
-  describe "get_trips method" do
+  describe "add trip method" do
     before do
-      @driver = RideShare::Driver.new(1, "Bernardo Prosacco", "WBWSS52P9NEYLVDE9")
-      @trips = RideShare::Trip.all_for_driver(@driver.id)
+      @pass = RideShare::Passenger.new(id: 1, name: "Ada", phone: "412-432-7640")
+      @driver = RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678")
+      trip = RideShare::Trip.new({id: 8, driver: @driver, passenger: @pass, date: "2016-08-08", rating: 5})
+
+      # @driver = RideShare::Driver.new(54, "Rogers Bartell IV", "1C9EVBRM0YBC564DZ")
     end
-    it "returns an array" do
-      @trips.must_be_kind_of Array
+
+    it "throws an argument error if trip is not provided" do
+      proc{ @driver.add_trip(1) }.must_raise ArgumentError
     end
-    it "all elements are Trip instances" do
-      @trips.each do |trip|
-        trip.must_be_kind_of RideShare::Trip
-      end
-    end
-    it "all Trips in array have same driver_id as id passed in" do
-      this_id = @driver.id
-      @trips.each do |trip|
-        trip.driver_id.must_equal this_id
-      end
+
+    it "increases the trip count by one" do
+      previous = @driver.trips.length
+      @driver.add_trip(RideShare::Trip.new(id: 55, passenger: @pass, driver: @driver))
+      @driver.trips.length.must_equal previous + 1
     end
   end
 
   describe "average_rating method" do
     before do
-      @driver = RideShare::Driver.new(54, "Rogers Bartell IV", "1C9EVBRM0YBC564DZ")
+      @driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV", vin: "1C9EVBRM0YBC564DZ")
+      trip = RideShare::Trip.new({id: 8, driver: @driver, passenger: nil, date: "2016-08-08", rating: 5})
+      @driver.add_trip(trip)
     end
     it "returns a float" do
       @driver.average_rating.must_be_kind_of Float
@@ -48,6 +53,10 @@ describe "Driver class" do
     it "float is <= 5.0" do
       average = @driver.average_rating
       average.must_be :<=, 5.0
+    end
+    it "returns zero if no trips" do
+      driver = RideShare::Driver.new(id: 54, name: "Rogers Bartell IV", vin: "1C9EVBRM0YBC564DZ")
+      driver.average_rating.must_equal 0
     end
   end
 end
