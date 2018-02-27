c9ceccf7e74def08420b21eb8c9cdd8fa5501e9a
Kari Bancroft <kariabancroft@gmail.com>
2018-02-26 22:39:05 -0800

    add more intialize tests for passenger class

--- specs/passenger_spec.rb
+++ specs/passenger_spec.rb
@@ -2,21 +2,43 @@ require_relative 'spec_helper'
 
 describe "Passenger class" do
 
-  it "is an instance of Passenger" do
-    test_rider = RideShare::Passenger.new({id: 1, name: "Smithy", phone_number: "353-533-5334"})
-    test_rider.must_be_kind_of RideShare::Passenger
+  describe "Passenger instantiation" do
+    before do
+      @passenger = RideShare::Passenger.new({id: 1, name: "Smithy", phone: "353-533-5334"})
+    end
+
+    it "is an instance of Passenger" do
+      @passenger.must_be_kind_of RideShare::Passenger
+    end
+
+    it "throws an argument error with a bad ID value" do
+      proc{ RideShare::Passenger.new(id: 0, name: "Smithy")}.must_raise ArgumentError
+    end
+
+    it "sets trips to an empty array if not provided" do
+      @passenger.trips.must_be_kind_of Array
+      @passenger.trips.length.must_equal 0
+    end
+
+    it "is set up for specific attributes and data types" do
+      [:id, :name, :phone_number, :trips].each do |prop|
+        @passenger.must_respond_to prop
+      end
+
+      @passenger.id.must_be_kind_of Integer
+      @passenger.name.must_be_kind_of String
+      @passenger.phone_number.must_be_kind_of String
+      @passenger.trips.must_be_kind_of Array
+    end
   end
+
 
   describe "trips property" do
     before do
-      @passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone_number: "1-602-620-2330 x3723", trips: [])
+      @passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723", trips: [])
       trip = RideShare::Trip.new({id: 8, driver: nil, passenger: @passenger, date: "2016-08-08", rating: 5})
 
       @passenger.add_trip(trip)
-    end
-
-    it "returns an array of trips" do
-      @passenger.trips.must_be_kind_of Array
     end
 
     it "each item in array is a Trip instance" do
@@ -34,7 +56,7 @@ describe "Passenger class" do
 
   describe "get_drivers method" do
     before do
-      @passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone_number: "1-602-620-2330 x3723")
+      @passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone: "1-602-620-2330 x3723")
       driver = RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678")
       trip = RideShare::Trip.new({id: 8, driver: driver, passenger: @passenger, date: "2016-08-08", rating: 5})
 
