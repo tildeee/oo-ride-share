ac89332ead730896312f512191c8953b4a9f227c
Kari Bancroft <kariabancroft@gmail.com>
2018-02-26 21:35:30 -0800

    definitely need to add the dispatcher file to the spec helper file

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -9,3 +9,4 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative '../lib/driver'
 require_relative '../lib/passenger'
 require_relative '../lib/trip'
+require_relative '../lib/trip_dispatcher'
