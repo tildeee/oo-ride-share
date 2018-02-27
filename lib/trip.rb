41310d26571e7f39aa9aeb2bb637b831d41e1d85
Kari Bancroft <kariabancroft@gmail.com>
2018-02-26 21:35:13 -0800

    remove most code from the trip class since it is not in the dispatcher

--- lib/trip.rb
+++ lib/trip.rb
@@ -4,44 +4,16 @@ require_relative 'passenger'
 
 module RideShare
   class Trip
-    attr_reader :passenger_id, :driver_id, :rating, :id, :date, :cost, :duration_hhmmss
+    attr_reader :passenger, :driver, :rating, :id, :date, :cost, :duration_hhmmss
 
     def initialize(input)
       @id = input[:id]
-      @driver_id = input[:driver_id]
-      @passenger_id = input[:passenger_id]
+      @driver = input[:driver]
+      @passenger = input[:passenger]
       @date = input[:date]
       @rating = input[:rating]
       @cost = input[:cost]
       @duration_hhmmss = input[:duration_hhmmss]
     end
-
-    def get_driver
-      return Driver.find(@driver_id)
-    end
-
-    def get_passenger
-      return Passenger.find(@passenger_id)
-    end
-
-    def self.all_for_driver(id)
-      list_of_trips = []
-      all_trips = Trip.all
-      all_trips.each do |trip|
-        list_of_trips << trip if trip.driver_id == id
-      end
-      return list_of_trips
-    end
-
-    def self.all_for_passenger(id)
-      list_of_trips = []
-      all_trips = Trip.all
-      all_trips.each do |trip|
-        list_of_trips << trip if trip.passenger_id == id
-      end
-      return list_of_trips
-    end
-
-
   end
 end
--- specs/trip_spec.rb
+++ specs/trip_spec.rb
@@ -2,32 +2,22 @@ require_relative 'spec_helper'
 
 describe "Trip class" do
 
-  it "is an instance of Trip" do
-    test_trip = RideShare::Trip.new({id: 1, passenger_id: 33, driver_id: 22, date: "01-02-0102", rating: 5})
-    test_trip.must_be_kind_of RideShare::Trip
-  end
-
-  describe "get_driver method" do
+  describe "initialize" do
     before do
-      @trip = RideShare::Trip.new({id: 8, driver_id: 93, passenger_id: 104, date: "2016-08-08", rating: 5})
+      pass = RideShare::Passenger.new(id: 1, name: "Ada", phone: "412-432-7640")
+      driver = RideShare::Driver.new(id: 3, name: "Lovelace", vin: "12345678912345678")
+      @trip = RideShare::Trip.new({id: 8, driver: driver, passenger: pass, date: "2016-08-08", rating: 5})
     end
-    it "returns an instance of Driver" do
-      @trip.get_driver.must_be_kind_of RideShare::Driver
+    it "is an instance of Trip" do
+      @trip.must_be_kind_of RideShare::Trip
     end
-    it "Driver's id matches driver_id on instance of Trip" do
-      @trip.get_driver.id.must_equal 93
-    end
-  end
 
-  describe "get_passenger method" do
-    before do
-      @trip = RideShare::Trip.new({id: 8, driver_id: 93, passenger_id: 104, date: "2016-08-08", rating: 5})
+    it "stores an instance of passenger" do
+      @trip.passenger.must_be_kind_of RideShare::Passenger
     end
-    it "returns an instance of Passenger" do
-      @trip.get_passenger.must_be_kind_of RideShare::Passenger
-    end
-    it "Passenger's id matches passenger_id on instance of Trip" do
-      @trip.get_passenger.id.must_equal 104
+
+    it "stores an instance of driver" do
+      @trip.driver.must_be_kind_of RideShare::Driver
     end
   end
 end
