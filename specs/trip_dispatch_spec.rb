d10bd23c6d418c3eaf63ecea356f980040d83507
Kari Bancroft <kariabancroft@gmail.com>
2018-02-26 22:11:32 -0800

    add tests for find methods

--- specs/trip_dispatch_spec.rb
+++ specs/trip_dispatch_spec.rb
@@ -18,4 +18,34 @@ describe "TripDispatcher class" do
       dispatcher.drivers.must_be_kind_of Array
     end
   end
+
+  describe "find_driver method" do
+    before do
+      @dispatcher = RideShare::TripDispatcher.new
+    end
+
+    it "throws an argument error for a bad ID" do
+      proc{ @dispatcher.find_driver(0) }.must_raise ArgumentError
+    end
+
+    it "finds a driver instance" do
+      driver = @dispatcher.find_driver(2)
+      driver.must_be_kind_of RideShare::Driver
+    end
+  end
+
+  describe "find_passenger method" do
+    before do
+      @dispatcher = RideShare::TripDispatcher.new
+    end
+
+    it "throws an argument error for a bad ID" do
+      proc{ @dispatcher.find_passenger(0) }.must_raise ArgumentError
+    end
+
+    it "finds a passenger instance" do
+      passenger = @dispatcher.find_passenger(2)
+      passenger.must_be_kind_of RideShare::Passenger
+    end
+  end
 end
