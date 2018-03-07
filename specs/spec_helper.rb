8b532dba9b3e790950beee67edc9e164cb19bb77
dee <dee@adadevelopersacademy.org>
2018-03-07 09:32:21 -0800

    remove comments in spec helper, add minitest pride

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -2,11 +2,10 @@ require 'time'
 require 'minitest'
 require 'minitest/autorun'
 require 'minitest/reporters'
-# Add simplecov
+require 'minitest/pride'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-# Require_relative your lib files here!
 require_relative '../lib/driver'
 require_relative '../lib/passenger'
 require_relative '../lib/trip'
